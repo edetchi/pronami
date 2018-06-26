1b20e4ec3acd5b5b8ba6bfca5dae50bf506d09be
root <root@localhost.localdomain>
2018-06-26 20:36:28 +0000

    4章の終わりまで完了

--- product/pro_branch.php
+++ product/pro_branch.php
@@ -1,10 +1,19 @@
 <?php
 
+session_start();
+session_regenerate_id(true);
+if (isset($_SESSION['login']) == false)
+{
+    print 'ログインされていません。<br>';
+    print '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
+    exit();
+}
+
 if (isset($_POST['disp']) == true)
 {
     if (isset($_POST['procode'])  == false)
     {
-        header('Location: staff_ng.php');
+        header('Location: pro_ng.php');
     }
     $pro_code = $_POST['procode'];
     header('Location: pro_disp.php?procode='.$pro_code);
--- staff_login/staff_top.php
+++ staff_login/staff_top.php
@@ -13,6 +13,7 @@ else
     print 'さんログイン中<br>';
     print '<br>';
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -25,8 +26,13 @@ else
 
 <body>
 
-商品が選択されていません。<br>
-<a href="pro_list.php">戻る</a>
+ショップ管理トップ<br>
+<br>
+<a href="../staff/staff_list.php">スタッフ管理</a><br>
+<br>
+<a href="../product/pro_list.php">商品管理</a><br>
+<br>
+<a href="staff_logout.php">ログアウト</a><br>
 
 </body>
 
