d0a10bd7c7689c8c912064231f19bbfa624b14f3
root <root@localhost.localdomain>
2018-06-27 04:35:19 +0000

    6-3まで完了

---
+++ shop/shop_cartin.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+session_regenerate_id(true);
+if (isset($_SESSION['member_login']) == false)
+{
+    print 'ようこそゲスト様<br>';
+    print '<a href="member_login.html">会員ログイン</a>';
+    print '<br>';
+}
+else
+{
+    print 'ようこそ';
+    print $_SESSION['member_name'];
+    print '様 ';
+    print '<a href="member_logout.html">ログアウト</a>';
+    print '<br>';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>ろくまる農園</title>
+</head>
+
+<body>
+
+<?php
+
+try
+{
+
+$pro_code = $_GET['procode'];
+
+if (isset($_SESSION['cart']) == true)
+{
+    $cart = $_SESSION['cart'];
+}
+
+$cart[] = $pro_code;
+$_SESSION['cart'] = $cart;
+
+}
+catch (Exception $e)
+{
+    print 'ただいま障害により大変ご迷惑お掛けしております。';
+    exit();
+}
+
+?>
+
+カートに追加しました。<br>
+<br>
+<a href="shop_list.php">商品一覧に戻る</a>
+
+</body>
+
+</html>
